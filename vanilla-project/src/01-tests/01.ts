3862da5cfa917e4fd9487836e38a6312153a9b7a
Mykola <nicholaskaradzha@gmail.com>
2022-02-08 18:00:07 +0700

    tests added

---
+++ vanilla-project/src/01-tests/01.ts
@@ -0,0 +1,17 @@
+
+export const sum = (a:number, b:number) => {
+    return (a+b)
+}
+export const mult = (a:number, b:number) => {
+    return (a*b)
+}
+
+export const wordsSplitting = (sentence:string) => {
+    const words = sentence.toLowerCase().split(' ');
+    return words.filter(w => w !== '' && w !== "-")
+        .map(w => w
+            .replace("!", "")
+            .replace(",", "")
+            .replace(".", "")
+            )
+}
